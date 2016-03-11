7584e5627496017e6c97b27a708b03a279f6f8c2
Luís Faceira <luis.faceira@beubi.com>
2016-03-11 13:20:58 -0800

    Added docker-compose with airfield

---
+++ airfield_settings.js
@@ -0,0 +1,14 @@
+exports.settings = {
+    username: "set AIRFIELD_USER in your env",
+    password: "set AIRFIELD_PASS in your env",
+    port: 3000,
+    cookieSecret: "thisissecret!$^&",
+    sessionSecret: "secretisthistoo^SSK@",
+    enable_openstack: false,
+    os_api_username: "admin",
+    os_api_password: "goodopenstackpassword",
+    os_api_tenant: "tenantidofinstances",
+    os_keystone_url: "http://10.0.0.1:5000",
+    os_nova_url: "http://10.0.0.1:8774",
+    REDIS_HOST: "redis"
+};
